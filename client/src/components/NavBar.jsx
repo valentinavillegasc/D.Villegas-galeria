f659f367bc0834ce02392afbee3a566fc03bbd8e
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-04 20:51:45 -0500

    burgerNav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Estilos/NavBar.module.css";
 import { Link } from "react-router-dom";
+import BurgerNav from "./BurgerNav";
+
 export default function NavBar() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [menuVisible, setMenuVisible] = useState(false);
+
   return (
     <div className={style.navBar}>
       <h2 className={style.logo}>D. Villegas</h2>
-      {/* <img src="url()" alt="logo" className={style} /> */}
-      <div className={style.buttons}>
-        <Link to="/home">
-          <button className={style.button}>Home</button>
+      <div
+        className={style.hamburgerMenu}
+        onClick={() => setIsVisible(!isVisible)}>
+        <button className={style.hamburgerMenuButton}>☰</button>
+      </div>
+      {isVisible && <BurgerNav />}
+      <div
+        className={`${style.buttons} ${
+          menuVisible ? style.showMenu : style.hideMenu
+        }`}>
+        <Link to="/home" className={style.button}>
+          Home
         </Link>
-        <Link to="/biografia">
-          <button className={style.button}>Biografía</button>
+        <Link to="/biografia" className={style.button}>
+          Biografía
         </Link>
       </div>
     </div>
