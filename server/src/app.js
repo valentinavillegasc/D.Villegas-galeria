ba6d12c515d389d46c7aab89dc0fbb6b10b43db5
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-11 16:09:51 -0500

    permitir peticiones

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,12 @@ const colibriRouter = require("./routes/colibriRouter");
 const coleccionRouter = require("./routes/ColeccionRouter");
 
 //Middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
