3264b49c76129cba92b17fa3fe94e532a4fc6eb2
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-14 14:03:55 -0500

    detail colibri

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from "./views/Landing";
 import Form from "./views/Form";
 import Colibries from "./views/Colibries";
 import DetailColeccion from "./views/DetailColeccion";
+import DetailColibri from "./views/DetailColibri";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/información" element={<Form />} />
         <Route path="/colibries" element={<Colibries />} />
         <Route path="/detailColeccion/:id" element={<DetailColeccion />} />
+        <Route path="/detailColibri/:id" element={<DetailColibri />} />
       </Routes>
     </div>
   );
