a0133b16bfa324bc5ce1abfca8678b518ac89e44
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-20 12:11:31 -0500

    Responsive 1

--- server/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/app");
-const { database } = require("./src/db");
-
-database.sync();
-app.listen(3001, () => {
-  console.log("Listening on port 3001!");
-});
--- server/src/controllers/Colecciones/getColeccionByName.js
+++
@@ -1,9 +0,0 @@
-const { Coleccion } = require("../../db");
-
-const getColeccionByName = async (name) => {
-  const colectionFiltered = await Coleccion.findAll({ where: { name } });
-  if (colectionFiltered) return colectionFiltered;
-  else return { error: `No hay colecciones con el nombre ${name}` };
-};
-
-module.exports = getColeccionByName;
