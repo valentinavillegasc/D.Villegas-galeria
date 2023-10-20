7fbe7c34c3cdb9bcf4ce320afc4636711582d9ad
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-20 12:22:59 -0500

    cambios2

--- server/src/handlers/coleccionHandler.js
+++
@@ -1,8 +0,0 @@
-// handlers/coleccionHandler.js
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
-const uploadMiddleware = upload.single("image");
-
-module.exports = uploadMiddleware;
