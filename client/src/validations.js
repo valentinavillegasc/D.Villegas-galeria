18b54007fd8793319940bdcb7cb0b6578ade1e90
Valentina Villegas <valentinavillegasc@hotmail.com>
2023-10-23 17:49:17 -0500

    Mensajes de error

--- client/src/validations.js
+++ client/src/validations.js
@@ -1,9 +1,9 @@
 const validation = (form) => {
   const errors = {};
   if (form.name && !/^[A-Z][a-zA-Z ]+$/.test(form.name))
-    errors.name = "Invalid name";
+    errors.name = "Nombre inválido";
   if (form.email && !/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(form.email))
-    errors.email = "Invalid email format";
+    errors.email = "Formato inválido";
   return errors;
 };
 
